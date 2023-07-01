7c6935e2b1bb6ab14f3b2ebb717ef4206ab58958
Marcin <Marcin.Kmetyk@gmail.com>
2023-07-01 11:58:30 +0200

    fix in Prime function

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ def isPrime (n):
 	if n<2:
 		return False
 
-	for i in range(2,n):
-		if n%2==0:
+	if n==2:
+		return True
+	
+	if n % 2==0:
+		return False
+
+	for i in range(3,int(n**0.5)+1,2):
+		if n%i==0:
 			return False
 		return True
+
 for i in range (20):
 	print (i,isPrime(i))
 
